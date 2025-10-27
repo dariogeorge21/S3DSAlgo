ae8e730c0fb785e5df38d494904b944aa2206cb5
Dario George <edu.dariogeorge21@gmail.com>
2025-10-27 20:54:17 +0530

    Refactor infix to postfix conversion logic and improve stack operations

--- experiments/03 - infix to postfix.c
+++ experiments/03 - infix to postfix.c
@@ -1,71 +1,91 @@
 // convert infix expression to postfix using stack.
 #include<stdio.h>
 #define limit 100
-#include<ctype.h>
 
-char STACK[limit];
-int top=-1;
+int stack[limit];
 
-char push(char c){
-	if (top<limit-1){
-		STACK[++top]=c;
+int top = -1;
+
+int push (char c){
+	if (top<=limit-1){
+		stack[++top]=c;
+		return 1;
 	}
-	return '\0';
+	else{
+		return 0;
+	}
 }
 
-char pop(){
+int pop(){
 	if (top>=0){
-		return STACK[top--];
+		return stack[top--];
 	}
-	return '\0';
+	else{
+		return -1;
+	}
 }
 
-char peek(){
+int peek(){
 	if (top>=0){
-		return STACK[top];
+		return stack[top];
 	}
-	return '\0';
+	else{
+		return -1;
+	}
 }
 
-int precedence(char op) {
-	if (op == '+' || op == '-') return 1;
-	if (op == '*' || op == '/') return 2;
-    	return 0;
+int isEmpty(){
+	if (top==-1){
+		return 1;
+	}
+	else{
+		return 0;
+	}
 }
 
-int main() {
-    	char infix[limit], postfix[limit];
-    	int i = 0, j = 0;
-    	char ch;
-	printf("Enter infix exp: ");
-   	scanf("%s", infix);
-
-    	while ((ch = infix[i++]) != '\0') {
-        	if (isalnum(ch)) {
-               		 postfix[j++] = ch;
-        	}
-        	else if (ch == '(') {
-            	push(ch);
-        	}
-        	else if (ch == ')') {
-            		while (top != -1 && peek() != '(') {
-                		postfix[j++] = pop();
-            		}
-            		pop();
-        		}
-       		 else {
-
-            		while (top != -1 && precedence(peek()) >= precedence(ch)) {
-                		postfix[j++] = pop();
-            		}
-            		push(ch);
-        	}
-    	}
-	while (top != -1) {
-		postfix[j++] = pop();
+int precedance(char op){
+	if (op=='^'){
+		return 3;
 	}
+	else if (op=='*' || op=='/'){
+		return 2;
+	}
+	else if (op=='+' || op=='-'){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
 
-   	postfix[j] = '\0';
-	printf("Postfix expression: %s\n", postfix);
+int main(){
+	char infix[limit],postfix[limit];
+	printf("Enter the infix expression: ");
+	scanf("%s",infix);
+	int j=0;
+	for (int i = 0; infix[i]!='\0'; ++i){
+		if (infix[i]>='a' && infix[i]<='z' || infix[i]>='A' && infix[i]<='Z'){
+			postfix[j++]=infix[i];
+		}
+		else if (infix[i]=='('){
+			push(infix[i]);
+		}
+		else if (infix[i]==')'){
+			while (peek()!='('){
+				postfix[j++]=pop();
+			}
+			pop();
+		}
+		else{
+			while (precedance(peek())>=precedance(infix[i])){
+				postfix[j++]=pop();
+			}
+			push(infix[i]);
+		}
+	}
+	while (!isEmpty()){
+		postfix[j++]=pop();
+	}
+	printf("Postfix expression: %s",postfix);
 	return 0;
 }
