a8b42c855524b3051b1b71fcd35def96c517dd6d
Dario George <edu.dariogeorge21@gmail.com>
2025-10-28 11:45:16 +0530

    Refactor polynomial sum implementation to use structured data for improved clarity and maintainability

--- experiments/01 - polynomials.c
+++ experiments/01 - polynomials.c
@@ -1,53 +1,68 @@
-// find the sum of 2 polynomials using an array!!
+// find the sum of 2 polynomials using an structure!!
 
-#include <stdio.h>
+#include<stdio.h>
 
-struct Term {
-    int coefficient;
-    int exponent;
+struct poly{
+    int coeff;
+    int exp;
 };
 
-typedef struct Term Term;
-
-
-void inputPolynomial(Term poly[], int *n) {
-    printf("Enter the number of terms in the polynomial: ");
-    scanf("%d", n);
-
-    for (int i = 0; i < *n; i++) {
-        printf("Enter coefficient and exponent for term %d: ", i + 1);
-        scanf("%d %d", &poly[i].coefficient, &poly[i].exponent);
+int main(){
+    int n1, n2;
+    printf("Enter the number of terms in the first polynomial: ");
+    scanf("%d", &n1);
+    struct poly p1[n1];
+    printf("Enter the coefficients and exponents of the first polynomial: ");
+    for(int i=0; i<n1; i++){
+        printf("Coefficient %d: ", i+1);
+        scanf("%d", &p1[i].coeff);
+        printf("Exponent %d: ", i+1);
+        scanf("%d", &p1[i].exp);
     }
-}
-
-
-void displayPolynomial(Term poly[], int n) {
-    for (int i = 0; i < n; i++) {
-        if (poly[i].coefficient != 0) {
-            if (i != 0 && poly[i].coefficient > 0) {
-                printf(" + ");
-            }
-            if (poly[i].exponent == 0) {
-                printf("%d", poly[i].coefficient);
-            } else if (poly[i].exponent == 1) {
-                printf("%dx", poly[i].coefficient);
-            } else {
-                printf("%dx^%d", poly[i].coefficient, poly[i].exponent);
+    printf("Enter the number of terms in the second polynomial: ");
+    scanf("%d", &n2);
+    struct poly p2[n2];
+    printf("Enter the coefficients and exponents of the second polynomial: ");
+    for(int i=0; i<n2; i++){
+        printf("Coefficient %d: ", i+1);
+        scanf("%d", &p2[i].coeff);
+        printf("Exponent %d: ", i+1);
+        scanf("%d", &p2[i].exp);
+    }
+    int max_exp;
+    for (int i =0,j=0;i<n1 && j<n2;i++,j++){
+        max_exp = (p1[i].exp > p2[j].exp) ? p1[i].exp : p2[j].exp;
+    }
+    struct poly sum[max_exp+1];
+    for (int i = 0; i <= max_exp; i++) {
+        sum[i].coeff = 0;
+        sum[i].exp = i;
+    }
+    for (int i = 0; i < max_exp; i++) {
+        if (p1[i].exp == p2[i].exp) {
+            sum[i].coeff = p1[i].coeff + p2[i].coeff;
+            sum[i].exp = p1[i].exp;
+        } else if (p1[i].exp > p2[i].exp) {
+            sum[i].coeff = p1[i].coeff;
+            sum[i].exp = p1[i].exp;
+        } else if (p1[i].exp < p2[i].exp) {
+            sum[i].coeff = p2[i].coeff;
+            sum[i].exp = p2[i].exp;
+        } else {
+            sum[i].coeff = 0;
+            sum[i].exp = 0;
+        }
+    }
+    printf("The sum of the polynomials is: ");
+    for(int i=0; i<max_exp; i++){
+        if(sum[i].coeff != 0){
+            printf("%dx^%d ", sum[i].coeff, sum[i].exp);
+            if(i<max_exp-1){
+                printf("+ ");
             }
         }
     }
     printf("\n");
-}
-
-int main() {
-    Term poly1[100];
-    int n1, n2;
-    printf("Enter the first polynomial:\n");
-    inputPolynomial(poly1, &n1);
-    printf("Enter the second polynomial:\n");
-    printf("\nPolynomial 1: ");
-    displayPolynomial(poly1, n1);
-    printf("Polynomial 2: ");
-
+    
     return 0;
 }
