fd4b661f998c2fb8fb131d1bceab26b72de629b8
Eldhose Abraham Emerson <eldhoseabrahamemerson2028@cs.sjcetpalai.ac.in>
2025-11-15 17:48:31 +0530

    code updated

--- experiments/03 - infix to postfix.c
+++ experiments/03 - infix to postfix.c
@@ -1,92 +1,64 @@
-// convert infix expression to postfix using stack.
-#include<stdio.h>
-#define limit 100
-
-int stack[limit];
-
-int top = -1;
-
-int push (char c){
-	if (top<=limit-1){
-		stack[++top]=c;
-		return 1;
-	}
-	else{
-		return 0;
-	}
-}
-
-int pop(){
-	if (top>=0){
-		return stack[top--];
-	}
-	else{
-		return -1;
-	}
-}
-
-int peek(){
-	if (top>=0){
-		return stack[top];
-	}
-	else{
-		return -1;
-	}
-}
-
-int isEmpty(){
-	if (top==-1){
-		return 1;
-	}
-	else{
-		return 0;
-	}
-}
-
-int precedance(char op){
-	if (op=='^'){
-		return 3;
-	}
-	else if (op=='*' || op=='/'){
-		return 2;
-	}
-	else if (op=='+' || op=='-'){
-		return 1;
-	}
-	else{
-		return 0;
-	}
-}
-
-int main(){
-	char infix[limit],postfix[limit];
-	printf("Enter the infix expression: ");
-	scanf("%s",infix);
-	int j=0;
-	for (int i = 0; infix[i]!='\0'; ++i){
-		if (infix[i]>='a' && infix[i]<='z' || infix[i]>='A' && infix[i]<='Z'){
-			postfix[j++]=infix[i];
-		}
-		else if (infix[i]=='('){
-			push(infix[i]);
-		}
-		else if (infix[i]==')'){
-			while (peek()!='('){
-				postfix[j++]=pop();
-			}
-			pop();
-		}
-		else{
-			while (precedance(peek())>=precedance(infix[i])){
-				postfix[j++]=pop();
-			}
-			push(infix[i]);
-		}
-	}
-	while (!isEmpty()){
-		postfix[j++]=pop();
-	}
-	postfix[j]='\0';
-	printf("Postfix expression: %s",postfix);
-	return 0;
-}
+#include<stdio.h> 
+#include<ctype.h> 
+ 
+char stack[100]; 
+int top = -1; 
+ 
+void push(char x) 
+{ 
+    stack[++top] = x; 
+} 
+ 
+char pop() 
+{ 
+    if(top == -1) 
+        return -1; 
+    else 
+        return stack[top--]; 
+} 
+ 
+int precedence(char x) 
+{ 
+    if(x == '(') 
+        return 0; 
+    if(x == '+' || x == '-') 
+        return 1; 
+    if(x == '*' || x == '/') 
+        return 2; 
+    return 0; 
+} 
+ 
+int main() 
+{ 
+    char exp[100]; 
+    char *e, x; 
+    printf("Enter the expression : "); 
+    scanf("%s",exp); 
+    printf("\n The converted expression is:"); 
+    e = exp; 
+    
+    while(*e != '\0') 
+    { 
+        if(isalnum(*e)) 
+            printf("%c ",*e); 
+        else if(*e == '(') 
+            push(*e); 
+        else if(*e == ')') 
+        { 
+            while((x = pop()) != '(') 
+                printf("%c ", x); 
+        } 
+        else 
+        { 
+            while(precedence(stack[top]) >= precedence(*e)) 
+                printf("%c ",pop()); 
+            push(*e); 
+        } 
+        e++; 
+    } 
+    
+    while(top != -1) 
+    { 
+        printf("%c ",pop()); 
+    }return 0; 
+} 
